08274f6ac646ad1d8aee8ad1a81ce67d82e1c203
anastasia2889 <anastasia.brodskaja@yandex.ru>
2023-04-14 22:08:58 +0300

    HW is done (streams).

---
+++ src/test/java/com/streams/streams5/Streams5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streams.streams5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Streams5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
